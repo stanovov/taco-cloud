b58dd2ed9da6414aaf052d44b1d1e51c06df66a9
semyon.stanovov <sestanovov@gmail.com>
2023-05-07 00:04:54 +0300

    2.1.2 Создание класса контроллера

--- src/main/java/sia/tacocloud/controller/web/HomeController.java
+++ src/main/java/sia/tacocloud/controller/web/HomeController.java
@@ -1,4 +1,4 @@
-package sia.tacocloud.controller;
+package sia.tacocloud.controller.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
