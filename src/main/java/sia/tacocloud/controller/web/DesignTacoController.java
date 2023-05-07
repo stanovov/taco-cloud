91ffe53c860656d7f17dba0bf07aa7b353198a2c
semyon.stanovov <sestanovov@gmail.com>
2023-05-07 23:33:13 +0300

    2.3.2 Выполнение проверки после привязки

--- src/main/java/sia/tacocloud/controller/web/DesignTacoController.java
+++ src/main/java/sia/tacocloud/controller/web/DesignTacoController.java
@@ -3,12 +3,14 @@ package sia.tacocloud.controller.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import sia.tacocloud.model.entity.Ingredient;
 import sia.tacocloud.model.entity.Taco;
 import sia.tacocloud.model.entity.TacoOrder;
 import sia.tacocloud.model.enums.IngredientType;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -55,7 +57,10 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}:", taco);
         return "redirect:/orders/current";
--- src/main/java/sia/tacocloud/controller/web/OrderController.java
+++ src/main/java/sia/tacocloud/controller/web/OrderController.java
@@ -2,12 +2,15 @@ package sia.tacocloud.controller.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import sia.tacocloud.model.entity.TacoOrder;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -21,7 +24,10 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
         return "redirect:/";
