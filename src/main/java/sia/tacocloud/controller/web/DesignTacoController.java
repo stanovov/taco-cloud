e1de3e52de55ca59d2ffc44110e4d3de09c2c711
semyon.stanovov <sestanovov@gmail.com>
2023-05-07 21:24:44 +0300

    2.2 Обработка отправленной формы

--- src/main/java/sia/tacocloud/controller/web/DesignTacoController.java
+++ src/main/java/sia/tacocloud/controller/web/DesignTacoController.java
@@ -3,10 +3,7 @@ package sia.tacocloud.controller.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import sia.tacocloud.model.entity.Ingredient;
 import sia.tacocloud.model.entity.Taco;
 import sia.tacocloud.model.entity.TacoOrder;
@@ -55,6 +52,13 @@ public class DesignTacoController {
     @GetMapping
     public String showDesignForm() {
         return "design";
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}:", taco);
+        return "redirect:/orders/current";
     }
 
     private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, IngredientType type) {
